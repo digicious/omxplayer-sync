527d2a07a98931900cb72c7d443357dfa6e692f6
digicious <francois.chasseur@gmail.com>
2015-07-10 18:58:52 +0000

    Fix installation script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo pip install pexpect
 sudo apt-get remove omxplayer
 sudo rm -rf /usr/bin/omxplayer /usr/bin/omxplayer.bin /usr/lib/omxplayer
 sudo wget -O- http://yokto.net/0/omxplayer/omxplayer-3176db4.tar.bz2 | tar -C / -xjvf -
-sudo ln -sf /lib/arm-linux-gnueabihf/libpcre.so.3 /lib/arm-lsudo inux-gnueabihf/libpcre.so.1
+sudo ln -sf /lib/arm-linux-gnueabihf/libpcre.so.3 /lib/arm-linux-gnueabihf/libpcre.so.1
 sudo wget -O /usr/bin/omxplayer-sync https://github.com/turingmachine/omxplayer-sync/raw/master/omxplayer-sync
 sudo chmod 0755 /usr/bin/omxplayer-sync
 sudo wget https://github.com/turingmachine/omxplayer-sync/raw/master/synctest.mp4
